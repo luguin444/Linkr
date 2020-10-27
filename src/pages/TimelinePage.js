a8a024a7cbe2fe9e175c1869e1dd8222ed17151b
hoffmannpaola <hoffmannpaola@gmail.com>
2020-10-27 15:06:51 -0300

    Layout newpost ok

--- src/pages/TimelinePage.js
+++ src/pages/TimelinePage.js
@@ -26,7 +26,25 @@ export default function TimelinePage () {
             </Title>
             <Container> 
                 <Posts> 
-                    Posts
+                    <NewPost>
+                        <Info>
+                            <Photo> 
+                                <img src="https://d26lpennugtm8s.cloudfront.net/stores/861/835/products/dobbt11-d0b3f13551c024bb4415358341779424-640-0.jpg" />
+                            </Photo>
+                            <ContainerPost>
+                                <h3> O que você tem pra favoritar hoje? </h3>
+                                <input placeholder="http://..." ></input>
+                                <textarea placeholder="Muito irado esse link falando de #javascript"  rows="4" cols="33"> </textarea>
+                            </ContainerPost>           
+                
+                        </Info>
+                        <Publish>
+                            <Button> Publish </Button>
+                        </Publish>
+                        
+                        
+
+                    </NewPost>
                 </Posts>
                 <Trending>
                     <div className="title">
@@ -89,7 +107,7 @@ const User = styled.div `
 `;
 
 const Main = styled.div ` 
-    width: 70%;
+    width: 60%;
     margin: auto;
     display: flex;
     flex-direction: column;
@@ -111,13 +129,6 @@ const Title = styled.div`
 const Container = styled.div` 
     width: 100%;
     display: flex;
-
-`;
-
-const Posts = styled.div ` 
-    width: 70%;
-    background: orange;
-
 
 `;
 
@@ -149,5 +160,92 @@ const Trending = styled.div `
         line-height: 1.3rem;
         font-weight: 700;
     }
+
+`;
+
+const Posts = styled.div ` 
+    width: 70%;
+    
+    
+`;
+
+const NewPost = styled.div `
+    width: 38.2rem;
+    background: white;
+    padding: 1rem;
+    border-radius: 1rem;
+    
+`;
+
+const Info = styled.div `
+    width: 100%;
+    display: flex;
+    
+
+`;
+
+const Photo = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+
+    img {
+        width: 3rem;
+        border-radius: 50%;
+        flex-shrink: 0;
+        margin-left: 1rem;
+
+    }
+    
+
+`;
+
+const Publish = styled.div `
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+
+`;
+
+const Button = styled.button `
+    background: #1877F2;
+    width: 7rem;
+    height: 2rem;
+    border-radius: 0.3rem;
+    border: 0;
+    margin-right: 1.3rem;
+   
+
+    &:focus {
+        outline: 0;
+    }
+
+
+`;
+
+const ContainerPost = styled.div `
+    width: 80%;
+    height: 10rem;
+    margin-left: 5%;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+
+    input {
+        background: #EFEFEF;
+        border-radius: 0.3rem;
+        border: 0;
+        margin: 1rem 0;
+        line-height: 2rem;
+    }
+
+    textarea {
+        background: #EFEFEF;
+        border-radius: 0.3rem;
+        border: 0;
+
+    }
+
+    
 
 `;
